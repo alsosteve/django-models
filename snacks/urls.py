581c9bef81867052144b401f2b90cfe00d60cc2f
Steve Ngo <stevehngo@hotmail.com>
2022-06-04 03:29:31 -0400

    update app urlpatterns to handle detail view

--- snacks/urls.py
+++ snacks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import SnackListView
+from .views import SnackListView, SnackDetailView
 
 urlpatterns = [
   # empty sub-path handled by SnackListView using as_view()
     path('', SnackListView.as_view(), name="snack_list"),
+    path('<int:pk>/', SnackDetailView.as_view(), name="snack_detail")
 ]
