6d61cd72617188482064ae0ee6b77b5533f454cc
Steve Ngo <stevehngo@hotmail.com>
2022-06-04 03:33:20 -0400

    detail view temp

--- snacks/urls.py
+++ snacks/urls.py
@@ -4,5 +4,6 @@ from .views import SnackListView, SnackDetailView
 urlpatterns = [
   # empty sub-path handled by SnackListView using as_view()
     path('', SnackListView.as_view(), name="snack_list"),
+    
     path('<int:pk>/', SnackDetailView.as_view(), name="snack_detail")
 ]
