fbb78ddab1d9844578aa019c494b5137f1c9c3b9
Steve Ngo <stevehngo@hotmail.com>
2022-06-04 03:26:48 -0400

    add detail view

--- snacks/views.py
+++ snacks/views.py
@@ -5,3 +5,7 @@ from .models import Snack
 class SnackListView(ListView):
   template_name = "snack_list.html"
   model = Snack
+
+class SnackDetailView(DetailView):
+    template_name = "snack_detail.html"
+    model = Snack
