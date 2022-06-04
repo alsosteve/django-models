9ee4d7f1e92291f1c0aac1da0dd93acffa860ead
Steve Ngo <stevehngo@hotmail.com>
2022-06-04 03:41:56 -0400

    links

--- snacks/urls.py
+++ snacks/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
   # empty sub-path handled by SnackListView using as_view()
     path('', SnackListView.as_view(), name="snack_list"),
     
-    path('<int:pk>/', SnackDetailView.as_view(), name="snack_detail")
+    path('<int:pk>/', SnackDetailView.as_view(), name="snack_detail"),
 ]
